3a5fa314c7f5335cb4f88e7ab5f0f88b5582a611
Dan Shelly <danshelly@gmail.com>
2014-04-28 08:21:53 +0300

    content

---
+++ InMemoryStoreTest/DSMasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  DSMasterViewController.h
+//  InMemoryStoreTest
+//
+//  Created by Dan Shelly on 28/4/2014.
+//  Copyright (c) 2014 SO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DSDetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface DSMasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DSDetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ InMemoryStoreTestTests/InMemoryStoreTestTests.m
@@ -0,0 +1,34 @@
+//
+//  InMemoryStoreTestTests.m
+//  InMemoryStoreTestTests
+//
+//  Created by Dan Shelly on 28/4/2014.
+//  Copyright (c) 2014 SO. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InMemoryStoreTestTests : XCTestCase
+
+@end
+
+@implementation InMemoryStoreTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
